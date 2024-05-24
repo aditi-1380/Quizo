c507bfc7d172dc8bba8921410e2f27d63328c550
aditi-1380 <guptaditi005@gmail.com>
2024-05-24 17:42:46 +0530

    projeect adeed

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Quizo
- 
+FEE PROJECT
---
+++ src/components/ui/NavBar/index.tsx
@@ -0,0 +1,13 @@
+import { SignInButton } from '@clerk/clerk-react'
+import React from 'react'
+import Home from '../../../pages/home'
+
+
+
+const NavBar = () => {
+  return (
+    <Home />
+  )
+}
+
+export default NavBar
